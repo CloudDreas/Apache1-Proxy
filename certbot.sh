e4271ffc08d7cb89351c52c951875f471a986afa
Andreas van Waveren <avwaveren@gmail.com>
2025-06-11 01:51:14 +0200

    upload change2

--- certbot.sh
+++ certbot.sh
@@ -1,4 +1,8 @@
-sudo certbot certonly \
-  --dns-cloudflare \
-  --dns-cloudflare-credentials ~/.secrets/certbot/cloudflare.ini \
-  -d '*.$DOMAIN_NAME' -d $DOMAIN_NAME
+#!/bin/bash
+source "$(dirname "$0")/config.sh"
+
+echo "Domain is $DOMAIN_NAME"
+# Use $DOMAIN_NAME wherever you need the domain
+
+certbot certonly --manual --preferred-challenges=dns \
+  -d "*.$DOMAIN_NAME" -d "$DOMAIN_NAME"
--- config.sh
+++ config.sh
@@ -1,2 +1,2 @@
 # config.sh
-DOMAIN_NAME="$DOMAIN_NAME"
+DOMAIN_NAME="vanwaverentech.nl"
